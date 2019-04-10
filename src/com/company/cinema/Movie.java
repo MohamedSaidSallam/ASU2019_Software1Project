6551a998d9896c626ff38de72ea3987cf50143e8
Mohamed <mohamed.xyz@hotmail.com>
2019-04-10 09:39:54 +0200

    refactor: removed unused imports in Movie

--- src/com/company/cinema/Movie.java
+++ src/com/company/cinema/Movie.java
@@ -1,17 +1,5 @@
 package com.company.cinema;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-//import javafx.scene.paint.Color;
-//import javafx.scene.text.Font;
-//import javafx.scene.text.FontWeight;
-
 public class Movie {
 
     public class Info {
