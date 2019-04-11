d07ec0641d359bf8ec1b0a51c4f58674fafd8a8b
Mohamed <mohamed.xyz@hotmail.com>
2019-04-11 18:04:33 +0200

    feat: implemented APIProto
    also added:
    - src\resources\json\APIData.json
    - .env to .gitignore

--- src/com/company/prototyping/APIProto.java
+++ src/com/company/prototyping/APIProto.java
@@ -1,4 +1,30 @@
 package com.company.prototyping;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class APIProto {
+
+    // region Static
+    private APIProto() {
+
+    }
+    // endregion Static
+
+    public static String getAPIData() {
+        String fileData = null;
+        try {
+            fileData = new String(Files.readAllBytes(Paths.get("src/resources/json/APIData.json")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileData;
+    }
+
+    public static void main(String[] args) {
+        //for Testing purposes
+
+        System.out.println(getAPIData());
+    }
 }
