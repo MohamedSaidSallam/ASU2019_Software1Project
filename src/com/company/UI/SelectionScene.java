cd41ba4b46b39317e44950987ece4abd46258d0b
Mohamed <mohamed.xyz@hotmail.com>
2019-04-14 04:19:10 +0200

    refactor: general refactor of SelectionScene

--- src/com/company/UI/SelectionScene.java
+++ src/com/company/UI/SelectionScene.java
@@ -18,37 +18,46 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SelectionScene extends Application {
+
+    // region Constants
+    private static final Color SEAT_UNAVAILABLE_COLOR = Color.LIGHTCORAL;
+    private static final Color SEAT_SELECTED_COLOR = Color.LIGHTSTEELBLUE;
+    private static final Color SEAT_NOT_SELECTED_COLOR = Color.WHITE;
+    private static final Color SEAT_STROKE_COLOR = Color.BLACK;
+    // endregion Constants
+
+    // region Variables
     private Stage window;
-    private static int  numberS = 0;
+    private int numberS = 0;
+    // endregion Variables
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-
         window = primaryStage;
 
-        Label lbl_noSeat = new Label("0");//todo change this line's location
+        Label lbl_noSeat = createOptionLabel("0");//todo change this line's location
 
         // region TESTING
         Seat seats[] = new Seat[100];
 
         int c = 1;
         char r = 'A';
-        for(int s=0;s<100;s++){
-          seats[s]= new Seat(String.valueOf(c),String.valueOf(r));
-          c++;
-          if((s+1)%10 == 0){
-              c = 1;
-              r++;
-          }
+        for (int s = 0; s < 100; s++) {
+            seats[s] = new Seat(String.valueOf(c), String.valueOf(r));
+            c++;
+            if ((s + 1) % 10 == 0) {
+                c = 1;
+                r++;
+            }
         }
-        Hall hall = new Hall(1,"Hall A",100,seats);
+        Hall hall = new Hall(1, "Hall A", 100, seats);
 
         //random seats
         seats[5].setAvailable(false);
@@ -59,20 +68,20 @@ public class SelectionScene extends Application {
         seats[80].setAvailable(false);
         seats[90].setAvailable(false);
 
-        Seat choice[];
+        Seat[] choice;
         choice = seats.clone();
         // endregion TESTING
 
         BorderPane brd_main = new BorderPane();
 
-        Scene scene = new Scene(brd_main); //todo set another layout to scene
+        Scene scene = new Scene(brd_main);
         scene.getStylesheets().add("file:src/resources/style.css");
 
         brd_main.getStyleClass().add("main");
 
-        // region Top
+        // region Header
         // region Content
-        ImageView img_logo = new ImageView(new Image("file:src/com/company/media/images/placeholder/logo.png"));
+        ImageView img_logo = new ImageView(new Image("file:src/resources/img/placeholder/logo.png"));
         img_logo.setFitHeight(75); //todo magicNumber
         img_logo.setFitWidth(75);  //todo magicNumber
 
@@ -98,153 +107,145 @@ public class SelectionScene extends Application {
 
         hbx_top.getChildren().addAll(img_logo, lbl_title, region, btn_temp1, btn_temp2);
         // endregion HBox
-        // endregion Top
+        // endregion Header
 
-        // region left
+        // region MoviePanel
+        Image img_moviePoster = new Image("file:src/resources/Interstellar_film_poster.jpg");
+        ImageView imgV_moviePoster = new ImageView(img_moviePoster);
+        imgV_moviePoster.fitHeightProperty().bind(window.heightProperty().multiply(0.63));
+        imgV_moviePoster.fitWidthProperty().bind(imgV_moviePoster.fitHeightProperty().multiply(img_moviePoster.getWidth() / img_moviePoster.getHeight()));
+
         Label lbl_movieTitle = new Label("Interstellar this is a very long title made for just testing purposes nothing else");
         lbl_movieTitle.getStyleClass().add("MovieTitleBig");
-        Image img_movie = new Image("file:src/resources/Interstellar_film_poster.jpg");
-        ImageView imageView = new ImageView(img_movie);
-        imageView.fitHeightProperty().bind(window.heightProperty().multiply(0.65));
-        imageView.fitWidthProperty().bind(imageView.fitHeightProperty().multiply(img_movie.getWidth() / img_movie.getHeight()));
-        lbl_movieTitle.prefWidthProperty().bind(imageView.fitWidthProperty());
+
+        lbl_movieTitle.prefWidthProperty().bind(imgV_moviePoster.fitWidthProperty());
         lbl_movieTitle.setWrapText(true);
 
         VBox vbx_left = new VBox();
-
         vbx_left.getStyleClass().add("temp1");
 
-        vbx_left.setPadding(new Insets(20, 20, 20, 20));
+        vbx_left.setPadding(new Insets(20, 20, 20, 20));//todo Magic Numbers
 
-        vbx_left.getChildren().addAll(imageView,lbl_movieTitle);
-        // endregion left
+        vbx_left.getChildren().addAll(imgV_moviePoster, lbl_movieTitle);
+        // endregion MoviePanel
 
-        // region Center
+        // region SeatSelection
 
         GridPane grd_seats = new GridPane();
 
         for (int j = 0; j < 10; j++) {
             for (int i = 0; i < 10; i++) {
-                int counter = 10*j + i;
-                Rectangle rectangle = new Rectangle(50, 50);
-                rectangle.setStroke(Color.BLACK);
-                rectangle.setStrokeWidth(3.0);
-                Text location = new Text(seats[(10*j+i)].getRow()+seats[(10*j+i)].getColumn());
-                if (!seats[(10 * j + i)].isAvailable()) {
-                    rectangle.setFill(Color.RED);
-                } else{
-                    rectangle.setFill(Color.WHITE);
-                    rectangle.setOnMouseClicked(e ->{
-                        rectangle.setWidth(50);
-                        rectangle.setHeight(50);
-                        if(rectangle.getFill()==Color.BLACK){
-                            rectangle.setFill(Color.WHITE);
-                            numberS--;
-                            lbl_noSeat.setText(String.valueOf(numberS));
-                        }
-                        else{
-                            rectangle.setFill(Color.BLACK);
-                            choice[counter].setAvailable(false);
-                            System.out.println(choice[counter].getColumn() + ' ' + choice[counter].getRow());
-                            numberS++;
-                            lbl_noSeat.setText(String.valueOf(numberS));
+                int counter = 10 * j + i;
+                Rectangle rectangle = new Rectangle();
 
+                rectangle.widthProperty().bind(window.widthProperty().multiply(0.035));
+                rectangle.heightProperty().bind(window.heightProperty().multiply(0.035));
+
+                rectangle.setStroke(SEAT_STROKE_COLOR);
+                rectangle.setStrokeWidth(3.0);
+
+                if (!seats[counter].isAvailable()) {
+                    rectangle.setFill(SEAT_UNAVAILABLE_COLOR);
+                } else {
+                    rectangle.setFill(SEAT_NOT_SELECTED_COLOR);
+
+                    rectangle.setOnMouseClicked(e -> {
+                        if (rectangle.getFill() == SEAT_SELECTED_COLOR) {
+                            rectangle.setFill(SEAT_NOT_SELECTED_COLOR);
+                            numberS--;
+                        } else {
+                            rectangle.setFill(SEAT_SELECTED_COLOR);
+                            choice[counter].setAvailable(false);
+                            numberS++;
                         }
+                        lbl_noSeat.setText(String.valueOf(numberS));
                     });
                 }
 
                 GridPane.setConstraints(rectangle, i, j);
-                GridPane.setConstraints(location,i,j);
-                GridPane.setHalignment(location, HPos.CENTER);
-//                Label lbl_seatTemp = new Label("temp");
-//                GridPane.setConstraints(lbl_seatTemp, i, j);
-//                grd_seats.getChildren().add(lbl_seatTemp);
-                grd_seats.getChildren().addAll(rectangle,location);
+
+                Label lbl_seatPos = new Label(seats[counter].getRow() + seats[counter].getColumn());
+                GridPane.setConstraints(lbl_seatPos, i, j);
+                GridPane.setHalignment(lbl_seatPos, HPos.CENTER);
+
+                grd_seats.getChildren().addAll(rectangle, lbl_seatPos);
             }
         }
-        grd_seats.setPadding(new Insets(20, 20, 20, 20));
-        grd_seats.setHgap(10);
-        grd_seats.setVgap(10);
+
+        grd_seats.setPadding(new Insets(20, 20, 20, 20)); //todo Magic Numbers
+        grd_seats.hgapProperty().bind(window.widthProperty().multiply(0.01));
+        grd_seats.vgapProperty().bind(window.heightProperty().multiply(0.01));
         grd_seats.setAlignment(Pos.BOTTOM_CENTER);
 
         grd_seats.getStyleClass().add("temp4");
+        // endregion SeatSelection
 
+        // region Options
 
-        // endregion Center
+        // region noSeats
+        Label lbl_stringSeats = createOptionLabel("Number of seats: ");
+        // endregion noSeats
 
-        // region Right
-
-        Label lbl_stringSeats = new Label("Number of seats: ");
-        lbl_stringSeats.getStyleClass().add("LabelOptions");
-        lbl_noSeat.getStyleClass().add("LabelOptions");
-        //region noSeats
-        HBox hbx_noSeats = new HBox();//todo check method
-        hbx_noSeats.getChildren().addAll(lbl_stringSeats, lbl_noSeat);
-        hbx_noSeats.setSpacing(10);//todo magic numbers
-        hbx_noSeats.setAlignment(Pos.CENTER);
-        //endregion noSeats
-
-        Label lbl_viewingOptions = new Label("Choose movie type");
-        lbl_viewingOptions.getStyleClass().add("LabelOptions");
+        // region viewingOptions
+        Label lbl_viewingOptions = createOptionLabel("Movie type:");
 
         ComboBox<ViewingOption> cbo_viewingOptions = new ComboBox<>();
-        cbo_viewingOptions.getItems().addAll(ViewingOption.Normal, ViewingOption.VIP, ViewingOption.IMAX, ViewingOption.Dolby);
-        cbo_viewingOptions.prefWidthProperty().bind(window.widthProperty().multiply(0.1));
-        cbo_viewingOptions.prefHeightProperty().bind(window.heightProperty().multiply(0.03));
 
+        cbo_viewingOptions.getItems().addAll(ViewingOption.values());
+        cbo_viewingOptions.getSelectionModel().selectFirst();
 
-        //region viewingOptions
-        HBox hbx_viewingOptions = new HBox();//todo check method
-        hbx_viewingOptions.getChildren().addAll(lbl_viewingOptions, cbo_viewingOptions);
-        hbx_viewingOptions.setSpacing(10);//todo magic numbers
-        hbx_viewingOptions.setAlignment(Pos.TOP_RIGHT);
-        //endregion viewingOptions
+        cbo_viewingOptions.prefWidthProperty().bind(window.widthProperty().multiply(0.13));
+        cbo_viewingOptions.prefHeightProperty().bind(window.heightProperty().multiply(0.04));
 
-        Label lbl_noGlasses = new Label("Number of needed glasses");
-        lbl_noGlasses.getStyleClass().add("LabelOptions");
+        cbo_viewingOptions.getStyleClass().add("NoFocus");
+        // endregion viewingOptions
+
+        // region noGlasses
+        Label lbl_noGlasses = createOptionLabel("Number of glasses:");
         lbl_noGlasses.setVisible(false);
 
-        ComboBox<Integer> cbo_noGlasses = new ComboBox<>();
-        cbo_noGlasses.setVisible(false);
-
-        //listener for viewing options comboBox
-        cbo_viewingOptions.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue) ->{
-            if(oldValue == ViewingOption.IMAX) {
-                lbl_noGlasses.setVisible(false);
-                cbo_noGlasses.setVisible(false);
-            }
-            else if(newValue == ViewingOption.IMAX) {
-                lbl_noGlasses.setVisible(true);
-                cbo_noGlasses.setVisible(true);
-            }
-        } );
-
-        cbo_noGlasses.prefWidthProperty().bind(window.widthProperty().multiply(0.1));
-        cbo_noGlasses.prefHeightProperty().bind(window.heightProperty().multiply(0.03));
-
-        //region noGlasses
         HBox hbx_noGlasses = new HBox();
-        hbx_noGlasses.getChildren().addAll(lbl_noGlasses, cbo_noGlasses);
-        hbx_noGlasses.setSpacing(10);//todo magic numbers
-        hbx_noGlasses.setAlignment(Pos.CENTER);
 
-        //endregion noGlasses
+        Label lbl_noGlassesValue = createOptionLabel("0");
+
+        Button btn_addGlasses = createButton("+",  0.01f, 0.05f);
+        btn_addGlasses.setOnAction(e -> lbl_noGlassesValue.setText(String.valueOf(Math.min(Integer.parseInt(lbl_noGlassesValue.getText()) + 1, numberS))));
+        btn_addGlasses.getStyleClass().add("NoFocus");
+
+        Button btn_removeGlasses = createButton("-", 0.01f, 0.05f);
+        btn_removeGlasses.setOnAction(e -> lbl_noGlassesValue.setText(String.valueOf(Math.max(Integer.parseInt(lbl_noGlassesValue.getText()) - 1, 0))));
+        btn_removeGlasses.getStyleClass().add( "NoFocus");
+
+        hbx_noGlasses.setAlignment(Pos.TOP_CENTER);
+        hbx_noGlasses.setSpacing(10);
+
+        hbx_noGlasses.getChildren().addAll(btn_addGlasses, btn_removeGlasses, lbl_noGlassesValue);
+        hbx_noGlasses.setVisible(false);
+
+        cbo_viewingOptions.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> {
+            if (newValue == ViewingOption.IMAX) {
+                lbl_noGlasses.setVisible(true);
+                hbx_noGlasses.setVisible(true);
+            }else{
+                lbl_noGlasses.setVisible(false);
+                hbx_noGlasses.setVisible(false);
+            }
+        });
+        // endregion noGlasses
 
         VBox vbx_right = new VBox();
-        vbx_right.getChildren().addAll(hbx_noSeats, hbx_viewingOptions, hbx_noGlasses);
-        vbx_right.setAlignment(Pos.TOP_CENTER);
-        vbx_right.setPadding(new Insets(10, 10, 10, 10));//todo magic numbers
-        vbx_right.setSpacing(10);//todo magic numbers
 
+        vbx_right.setAlignment(Pos.TOP_CENTER);
         vbx_right.getStyleClass().add("temp3");
-        // endregion Right
+
+        vbx_right.setPadding(new Insets(10, 10, 10, 10));//todo magic numbers
+        vbx_right.spacingProperty().bind(window.heightProperty().multiply(0.01));
+
+        vbx_right.getChildren().addAll(lbl_stringSeats, lbl_noSeat, lbl_viewingOptions, cbo_viewingOptions, lbl_noGlasses, hbx_noGlasses);
+        // endregion Options
 
         // region Footer
-        Button btn_btm = new Button("Next");
-        btn_btm.getStyleClass().add("btn");
-
-        btn_btm.prefWidthProperty().bind(window.widthProperty().multiply(0.1));
-        btn_btm.prefHeightProperty().bind(window.heightProperty().multiply(0.05));
+        Button btn_btm = createButton("Next",  0.1f, 0.05f);
 
         HBox hbx_btm = new HBox();
 
@@ -256,7 +257,7 @@ public class SelectionScene extends Application {
 
         hbx_btm.getStyleClass().add("temp2");
 
-        btn_btm.setOnAction(e ->{
+        btn_btm.setOnAction(e -> {
 
             System.out.println("**********");
         });//todo remove this later
@@ -268,9 +269,28 @@ public class SelectionScene extends Application {
         brd_main.setRight(vbx_right);
         brd_main.setBottom(hbx_btm);
 
-        primaryStage.setFullScreen(true);
-        primaryStage.setTitle("CTD");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window.setFullScreen(true);
+        window.setTitle("CTD");
+        window.setScene(scene);
+        window.show();
+    }
+
+    private Label createOptionLabel(String text){
+        Label label = new Label(text);
+
+        label.getStyleClass().add("LabelOptions");
+
+        return label;
+    }
+
+    private Button createButton(String text, float widthScale, float heightScale){
+        Button button = new Button(text);
+
+        button.getStyleClass().add("btn");
+
+        button.prefWidthProperty().bind(window.widthProperty().multiply(widthScale));
+        button.prefHeightProperty().bind(window.heightProperty().multiply(heightScale));
+
+        return button;
     }
 }
